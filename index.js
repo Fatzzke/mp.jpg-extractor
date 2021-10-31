d8cff76ce68e79f725bf50c59a6d079516917a8a
Fatzzke <36483549+Fatzzke@users.noreply.github.com>
2021-10-31 16:36:06 +0100

    add isom file type

--- index.js
+++ index.js
@@ -30,31 +30,71 @@ function readfile(element) {
 }
 
 function getVideoIndex(filearray) {
-    let searchpattern = [0x66, 0x74, 0x79, 0x70, 0x6d, 0x70, 0x34, 0x32] // ftypmp42 first 4 bytes omitted for a better search pattern
+    let fileTypeIsom = [0x69, 0x73, 0x6f, 0x6d];
+    let fileTyoemp42 = [0x6d, 0x70, 0x34, 0x32];
+    let found = false;
+    let index = 0;
+    // getting all ftyp indexes
+    do{
+    index = getFtypeIndex(index, filearray);
+        if(index == -1)
+            return -1;
+    //checking if its a matching type
 
-    //naive approach 
-    let index = 0
+        //mp42
+        for(let i = 0; i < fileTyoemp42.length; i++){
+            if(filearray[index + i + 4] != fileTyoemp42[i]){
+                break;
+            }
+            if(i == fileTyoemp42.length -1){
+                console.log("type mp42 found")
+                found = true;
+                return index - 4;
+            }
+        }
+
+        //isom
+        for(let i = 0; i < fileTypeIsom.length; i++){
+            if(filearray[index + i + 4] != fileTypeIsom[i]){
+                break;
+            }
+            if(i == fileTypeIsom.length -1){
+                console.log("type Isom found")
+                found = true;
+                return index - 4;
+            }
+        }
+
+    } while(index != -1 ||  !found)
+
+    return -1;
+}
+
+
+//returns index of FTYP. -1 If not found
+//naive approach 
+function getFtypeIndex(startIndex, filearray){
+    let index = startIndex;
+    let searchFTYP = [0x66, 0x74, 0x79, 0x70]; // ftyp search pattern
     let found = false;
     do {
-        index = filearray.indexOf(searchpattern[0], index)
+        index = filearray.indexOf(searchFTYP[0], index)
         if (index == -1) {
             console.log("video not found")
             return -1
-            break;
         }
-        for (let i = 1; i < searchpattern.length; i++) {
+        for (let i = 1; i < searchFTYP.length; i++) {
 
-            if (filearray[index + i] != searchpattern[i]) {
+            if (filearray[index + i] != searchFTYP[i]) {
                 index++;
                 break;
             }
-            if (i == searchpattern.length - 1) {
-                console.log("pattern found: " + index);
+            if (i == searchFTYP.length - 1) {
+                console.log("ftype found: " + index);
                 found = true;
             }
         }
 
     } while (!found)
-
-    return index = index - 4 //because we ignored the first 4 bytes in the pattern
+    return index 
 }
